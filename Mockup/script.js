4f7ef6c84342dd63165fc5ddea3a98331fb2d600
Arjhay Frias <prehacks0942@gmail.com>
2023-01-12 11:56:10 +0800

    Added Hero Section and Responsive Design

---
+++ Mockup/script.js
@@ -0,0 +1,34 @@
+
+$(document).ready(() => {
+    // On Screen Resize, Toggle Navbar Elements
+
+    $('#burger_button').click(() => {
+        if (!($('.clicked')[0])) {
+            $('nav').addClass('clicked');
+        } else {
+            $('.clicked').removeClass('clicked');
+        }
+
+        toggleElements();
+    });
+
+    // On Screen Resize, Toggle Navbar Elements
+    $(window).on("resize", (event) => {
+        toggleElements();
+    });
+});
+
+function toggleElements() {
+    if($(window).width() < 768 && !($('.clicked')[0])) {
+        $('.cta_holder > p, .cta_holder > img').show()
+        $('.gradient_background').show();
+    } else if($(window).width() < 768 && $('.clicked')[0]) {
+        $('.gradient_background').hide();
+        $('.clicked').show();
+        $('.cta_holder > p, .cta_holder > img').hide()
+    } else if($(window).width() >= 768 && $('.clicked')[0]) {
+        $('.clicked').removeClass('clicked');
+        $('.gradient_background').show();
+        $('.cta_holder > p, .cta_holder > img').show()
+    }
+}
