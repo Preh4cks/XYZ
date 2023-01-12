2a547fe6fd308c087a546988244bc21a4371dac5
Arjhay Frias <prehacks0942@gmail.com>
2023-01-12 17:51:10 +0800

    Added Reason Section

--- Mockup/script.js
+++ Mockup/script.js
@@ -21,14 +21,14 @@ $(document).ready(() => {
 function toggleElements() {
     if($(window).width() < 768 && !($('.clicked')[0])) {
         $('.cta_holder > p, .cta_holder > img').show()
-        $('.gradient_background, section').show();
+        $('.gradient_background, section, .section_2').show();
     } else if($(window).width() < 768 && $('.clicked')[0]) {
-        $('.gradient_background, section').hide();
+        $('.gradient_background, section, .section_2').hide();
         $('.clicked').show();
         $('.cta_holder > p, .cta_holder > img').hide()
     } else if($(window).width() >= 768 && $('.clicked')[0]) {
         $('.clicked').removeClass('clicked');
-        $('.gradient_background, section').show();
+        $('.gradient_background, section, .section_2').show();
         $('.cta_holder > p, .cta_holder > img').show()
     }
 }
