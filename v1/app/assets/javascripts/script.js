029a139643a656e8a8d8540c6d7e3f872d397539
Arjhay Frias <prehacks0942@gmail.com>
2023-01-16 10:53:36 +0800

    Separate each page partials

---
+++ v1/app/assets/javascripts/script.js
@@ -0,0 +1,71 @@
+class InitListeners {
+    constructor() {
+        this.#initBurgerClick();
+        this.#initOnScreenResize();
+        this.#initDropdownClick();
+    }
+
+    /** DOCU: Set Click Listener for Burger Button */
+    #initBurgerClick() {
+        if($('#burger_button')) {
+            $('#burger_button').click(() => {
+                if (!($('.clicked')[0])) {
+                    $('nav').addClass('clicked');
+                } else {
+                    $('.clicked').removeClass('clicked');
+                }
+    
+                this.#toggleElements();
+            });
+        }
+    }
+    
+    /** DOCU: Set Click Resize Listener for window */
+    #initOnScreenResize() {
+        $(window).on("resize", (event) => {
+            this.#toggleElements();
+        });
+    }
+
+    /** DOCU: Set Click Listener for DropdownMenu */
+    #initDropdownClick() {
+        if($(".dropdown > h3")) {
+            $(".dropdown > h3").click((e) => {
+                // If already clicked
+                let span_text = $(e.target).children("span").text();
+                if (span_text == "⌄") {
+                    $(e.target).siblings("p").slideToggle();
+                    $(".dropdown > h3").children("span").text("⌃");
+                    return;
+                }
+    
+                // Reset All
+                $(".dropdown > h3").siblings("p").hide();
+                $(".dropdown > h3").children("span").text("⌃");
+                
+                $(e.target).siblings("p").slideToggle();
+                $(e.target).children("span").text("⌄");
+            });
+        }
+    }
+
+    /** DOCU: Set Click Listener for Burger Button */
+    #toggleElements() {
+        if($(window).width() <= 768 && !($('.clicked')[0])) {
+            $('.cta_holder > p, .cta_holder > img').show()
+            $('.gradient_background, section, .section_2').show();
+        } else if($(window).width() <= 768 && $('.clicked')[0]) {
+            $('.gradient_background, section, .section_2').hide();
+            $('.clicked').show();
+            $('.cta_holder > p, .cta_holder > img').hide()
+        } else if($(window).width() >= 768 && $('.clicked')[0]) {
+            $('.clicked').removeClass('clicked');
+            $('.gradient_background, section, .section_2').show();
+            $('.cta_holder > p, .cta_holder > img').show()
+        }
+    }
+}
+
+$(document).ready(() => {
+    const init_listeners = new InitListeners;
+});
