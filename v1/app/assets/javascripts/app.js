17a3e57c513151f34696a8aab638a0d617a7b959
Arjhay Frias <prehacks0942@gmail.com>
2023-01-16 16:48:25 +0800

    Convert CSS to SCSS, bugfix[0002]: JQuery Doesn't work after Redirection

--- v1/app/assets/javascripts/app.js
+++ v1/app/assets/javascripts/app.js
@@ -1,3 +1,7 @@
+//= require jquery3
+//= require jquery_ujs
+//= require_tree .
+
 class InitListeners {
     constructor() {
         this.#initBurgerClick();
@@ -53,19 +57,19 @@ class InitListeners {
     #toggleElements() {
         if($(window).width() <= 768 && !($('.clicked')[0])) {
             $('.cta_holder > p, .cta_holder > img').show()
-            $('.gradient_background, section, .section_2').show();
+            $('.gradient_background, section, .section_2, footer').show();
         } else if($(window).width() <= 768 && $('.clicked')[0]) {
-            $('.gradient_background, section, .section_2').hide();
+            $('.gradient_background, section, .section_2, footer').hide();
             $('.clicked').show();
             $('.cta_holder > p, .cta_holder > img').hide()
         } else if($(window).width() >= 768 && $('.clicked')[0]) {
             $('.clicked').removeClass('clicked');
-            $('.gradient_background, section, .section_2').show();
+            $('.gradient_background, section, .section_2, footer').show();
             $('.cta_holder > p, .cta_holder > img').show()
         }
     }
 }
 
-$(document).ready(() => {
+$(document).ready(function() {
     const init_listeners = new InitListeners;
 });
